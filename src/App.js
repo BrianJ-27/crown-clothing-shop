63500c2b8b89f9c2d217022c17cbd8dcdd30a92b
Brian Johnson <johnson8727@gmail.com>
2022-01-11 10:05:05 -0500

    working with V6 react router syntax

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import './App.scss';
-import HomePage from './pages/homepage/Homepage.component';
+import React from "react";
+import "./App.scss";
+import HomePage from "./pages/homepage/Homepage.component";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
-
-
+const HatsPage = () => {
   return (
-    <div >
-     <HomePage/>
+    <div>
+      <h1>HATS PAGE</h1>
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/hats" element={<HatsPage />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
