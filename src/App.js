271e4a7cf872d35b9185352ef5eb30c75e414ab7
Brian Johnson <johnson8727@hotmail.com>
2022-01-12 15:03:28 -0500

    updating our app with a shop page, collection preview component and a collection item component

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={ShopPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
