350a430e8393c9c47e9313d443f520d170a0e408
Brian Johnson <johnson8727@hotmail.com>
2022-01-12 21:10:58 -0500

    added sign-in-and-register and signin components along with their stylesheets to the app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.scss";
 import HomePage from "./pages/homepage/Homepage.component";
 import ShopPage from "./pages/shop/Shop.component";
 import Header from "./components/header/Header.component";
+import SignInAndRegisterPage from "./pages/sign-in-and-register/sign-in-and-register.component";
 import { Switch, Route } from "react-router-dom";
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndRegisterPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-register/sign-in-and-register.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+import SignIn from "../../components/sign-in/Sign-in.component";
+import "./sign-in-and-register.styles.scss";
+
+const SignInAndRegisterPage = () => (
+  <div className="sign-in-and-register">
+    <SignIn />
+  </div>
+);
+
+export default SignInAndRegisterPage;
