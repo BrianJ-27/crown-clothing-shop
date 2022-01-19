c7c668ff507e4f3817f0f32a762b76def61f3c76
Brian Johnson <johnson8727@hotmail.com>
2022-01-19 10:56:16 -0500

    coded out sign-in and sign-out functionality in our app. also created styles for google sign-in button

--- src/components/sign-in/Sign-in.component.js
+++ src/components/sign-in/Sign-in.component.js
@@ -48,12 +48,13 @@ class SignIn extends React.Component {
             label="password"
             required
           />
-
-          <CustomButton type="submit">SIGN IN</CustomButton>
-          <CustomButton onClick={signInWithGoogle}>
-            {""}
-            SIGN IN WITH GOOGLE{""}
-          </CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit">SIGN IN</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              {""}
+              SIGN IN WITH GOOGLE{""}
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
